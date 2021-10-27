0d9575c034df933993b23240691ad9c0681f88cc
midoks <midoks@163.com>
2021-10-27 15:31:42 +0800

    yo

---
+++ dagger-mac/build/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 打包
+
+BUILD_DIR=$(cd "$(dirname "$0")"; pwd)
+ROOT_DIR=$(dirname "$BUILD_DIR")
+
+APP_NAME="dagger"
+APP_VER=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ROOT_DIR/dagger/dagger.xcodeproj/project.pbxproj)
+DAGGER_RELEASE=${BUILD_DIR}/release
+
+mkdir -p $DAGGER_RELEASE
+
+
+function build(){
+	echo "build dagger."${APP_VER}
+
+	echo "Building archive... please wait a minute"
+    xcodebuild -project $ROOT_DIR/dagger/dagger.xcodeproj -config Release -scheme dagger -archivePath ${DAGGER_RELEASE} archive
+
+
+
+}
+
+
+echo $ROOT_DIR
+
+build
