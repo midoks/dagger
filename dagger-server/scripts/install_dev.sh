344f0607272f12bdf51d86213fe4a1a489def91a
midoks <midoks@163.com>
2021-10-28 01:28:00 +0800

    Update install_dev.sh

--- dagger-server/scripts/install_dev.sh
+++ dagger-server/scripts/install_dev.sh
@@ -2,35 +2,37 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
 
-TAGRT_DIR=/usr/local/imail_dev
+TAGRT_DIR=/usr/local/dagger_dev
 mkdir -p $TAGRT_DIR
 cd $TAGRT_DIR
 
 
-if [ ! -d $TAGRT_DIR/imail ]; then
-	git clone https://github.com/midoks/imail
-	cd $TAGRT_DIR/imail
+if [ ! -d $TAGRT_DIR/dagger ]; then
+	git clone https://github.com/midoks/dagger
+	cd $TAGRT_DIR/dagger
 else
-	cd $TAGRT_DIR/imail
-	git pull https://github.com/midoks/imail
+	cd $TAGRT_DIR/dagger
+	git pull https://github.com/midoks/dagger
 fi
+
+cd $TAGRT_DIR/dagger/dagger-server
 
 go mod tidy
 go mod vendor
 
 
-rm -rf imail
+rm -rf dagger
 go build ./
 
 
-cd $TAGRT_DIR/imail/scripts
+cd $TAGRT_DIR/dagger/scripts
 
 sh make.sh
 
 systemctl daemon-reload
 
-service imail restart
+service dagger restart
 
-cd $TAGRT_DIR/imail && ./imail -v
+cd $TAGRT_DIR/dagger && ./dagger -v
 
 
