168ae3ecc69b2fd6630dcc0577ceba9e5542c200
midoks <midoks@163.com>
2022-05-04 17:44:59 +0800

    Update cmd_test.go

--- dagger-server/internal/cmd/cmd_test.go
+++ dagger-server/internal/cmd/cmd_test.go
@@ -30,6 +30,7 @@ func init() {
 	fmt.Println("mock")
 }
 
+// go test -bench=BenchmarkServerWS -benchmem -benchtime=1s -memprofile mem.out
 // go test -bench=BenchmarkServerWS -benchmem -benchtime=1s
 func BenchmarkServerWS(b *testing.B) {
 	router := gin.New()
