83f58b2f34ae7cb944be50589f310dfc45bd816a
midoks <midoks@163.com>
2023-03-08 23:38:31 +0800

    Update test.sh

--- dagger-client-http/test.sh
+++ dagger-client-http/test.sh
@@ -1,1 +1,3 @@
-/usr/local/Cellar/curl/7.79.1_1/bin/curl --proxy 127.0.0.1:1198 https://www.cachecha.com
+curl --proxy 127.0.0.1:198 https://www.cachecha.com
+
+curl -x "socks5://127.0.0.1:1060"  https://www.google.com
